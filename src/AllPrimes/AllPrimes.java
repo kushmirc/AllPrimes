596d9da567ac259b518536042a1a8679400786e7
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-08-26 10:24:37 -0600

    Create first instance, and call the isPrime method on it

--- src/AllPrimes/AllPrimes.java
+++ src/AllPrimes/AllPrimes.java
@@ -3,7 +3,11 @@ import java.util.ArrayList;
 
 public class AllPrimes {
     public boolean isPrime(int number) {
-
+        if (number == 2) {
+            return true;
+        }   else if (number < 2) {
+            return false;
+        }
     for (int i = 2; i < number; i++) {
 
         if (number % i == 0) {
@@ -13,6 +17,8 @@ public class AllPrimes {
       return true;
 }
     public static void main(String[] args) {
+        AllPrimes pr = new AllPrimes();
 
+        System.out.println(pr.isPrime(14));
     }
 }
