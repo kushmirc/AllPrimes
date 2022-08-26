9bba03648bbb9b6b4d3f22da1d1faf9db41ba70d
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-08-26 10:17:38 -0600

    Add isPrime method with conditional to test if a given number is prime or not

--- src/AllPrimes/AllPrimes.java
+++ src/AllPrimes/AllPrimes.java
@@ -2,7 +2,16 @@ package AllPrimes;
 import java.util.ArrayList;
 
 public class AllPrimes {
+    public boolean isPrime(int number) {
 
+    for (int i = 2; i < number; i++) {
+
+        if (number % i == 0) {
+            return false;
+        }
+    }
+      return true;
+}
     public static void main(String[] args) {
 
     }
